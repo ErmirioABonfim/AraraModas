4c33f5b733787ede4f42f08d882c061195dbea72
Ermirio Alves Bonfim <ermirio.contato@gmail.com>
2020-11-25 10:02:07 -0300

    Add project files.

---
+++ AraraModas/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace AraraModas
+{
+    partial class frmCadastroDeProdutos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastroDeProdutos));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNomeProduto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbxDescricao = new System.Windows.Forms.ComboBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lsbProdutos = new System.Windows.Forms.ListView();
+            this.cbxSexo = new System.Windows.Forms.ComboBox();
+            this.cbxTamanho = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome Produto:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNomeProduto
+            // 
+            this.txtNomeProduto.Location = new System.Drawing.Point(12, 50);
+            this.txtNomeProduto.Name = "txtNomeProduto";
+            this.txtNomeProduto.Size = new System.Drawing.Size(339, 22);
+            this.txtNomeProduto.TabIndex = 1;
+            this.txtNomeProduto.TextChanged += new System.EventHandler(this.txtNomeProduto_TextChanged);
+            this.txtNomeProduto.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtNomeProduto_KeyUp);
+            this.txtNomeProduto.Validating += new System.ComponentModel.CancelEventHandler(this.txtNomeProduto_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(359, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cor:";
+            // 
+            // txtCor
+            // 
+            this.txtCor.Location = new System.Drawing.Point(359, 50);
+            this.txtCor.Name = "txtCor";
+            this.txtCor.Size = new System.Drawing.Size(126, 22);
+            this.txtCor.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(507, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sexo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(600, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tamanho";
+            // 
+            // cbxDescricao
+            // 
+            this.cbxDescricao.FormattingEnabled = true;
+            this.cbxDescricao.Location = new System.Drawing.Point(12, 98);
+            this.cbxDescricao.Name = "cbxDescricao";
+            this.cbxDescricao.Size = new System.Drawing.Size(1027, 24);
+            this.cbxDescricao.TabIndex = 8;
+            this.cbxDescricao.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.cbxDescricao.Validating += new System.ComponentModel.CancelEventHandler(this.cbxDescricao_Validating);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnInserir.Location = new System.Drawing.Point(740, 38);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(88, 34);
+            this.btnInserir.TabIndex = 10;
+            this.btnInserir.Text = "INSERIR";
+            this.btnInserir.UseVisualStyleBackColor = false;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click_1);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(834, 38);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(86, 34);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(926, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 44);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Gravar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "DESCRIÇÃO";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lsbProdutos
+            // 
+            this.lsbProdutos.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lsbProdutos.FullRowSelect = true;
+            this.lsbProdutos.GridLines = true;
+            this.lsbProdutos.HideSelection = false;
+            this.lsbProdutos.LabelEdit = true;
+            this.lsbProdutos.Location = new System.Drawing.Point(12, 128);
+            this.lsbProdutos.Name = "lsbProdutos";
+            this.lsbProdutos.Size = new System.Drawing.Size(1027, 286);
+            this.lsbProdutos.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lsbProdutos.TabIndex = 14;
+            this.lsbProdutos.UseCompatibleStateImageBehavior = false;
+            this.lsbProdutos.View = System.Windows.Forms.View.Details;
+            this.lsbProdutos.Validating += new System.ComponentModel.CancelEventHandler(this.lsbProdutos_Validating);
+            // 
+            // cbxSexo
+            // 
+            this.cbxSexo.FormattingEnabled = true;
+            this.cbxSexo.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cbxSexo.Location = new System.Drawing.Point(507, 49);
+            this.cbxSexo.Name = "cbxSexo";
+            this.cbxSexo.Size = new System.Drawing.Size(74, 24);
+            this.cbxSexo.TabIndex = 15;
+            this.cbxSexo.SelectedIndexChanged += new System.EventHandler(this.cbxSexo_SelectedIndexChanged);
+            // 
+            // cbxTamanho
+            // 
+            this.cbxTamanho.FormattingEnabled = true;
+            this.cbxTamanho.Items.AddRange(new object[] {
+            "GG",
+            "G",
+            "M",
+            "P",
+            "PP"});
+            this.cbxTamanho.Location = new System.Drawing.Point(600, 49);
+            this.cbxTamanho.Name = "cbxTamanho";
+            this.cbxTamanho.Size = new System.Drawing.Size(121, 24);
+            this.cbxTamanho.TabIndex = 16;
+            // 
+            // frmCadastroDeProdutos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1040, 423);
+            this.Controls.Add(this.cbxTamanho);
+            this.Controls.Add(this.cbxSexo);
+            this.Controls.Add(this.lsbProdutos);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.cbxDescricao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNomeProduto);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCadastroDeProdutos";
+            this.Text = "Cadastro De Produtos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNomeProduto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbxDescricao;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView lsbProdutos;
+        private System.Windows.Forms.ComboBox cbxSexo;
+        private System.Windows.Forms.ComboBox cbxTamanho;
+    }
+}
+
---
+++ AraraModas/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AraraModas
+{
+    public partial class frmCadastroDeProdutos : Form
+    {
+        public frmCadastroDeProdutos()
+        {
+            InitializeComponent();
+            CriarColunas();
+        }
+
+        int iSelecionado = -1;
+        private void CriarColunas()
+        {
+            lsbProdutos.Clear();
+            lsbProdutos.Columns.Insert(0, "Nome", 250, HorizontalAlignment.Left );
+            lsbProdutos.Columns.Insert(1, "Cor", 250, HorizontalAlignment.Left);
+            lsbProdutos.Columns.Insert(2, "Sexo", 50, HorizontalAlignment.Left);
+            lsbProdutos.Columns.Insert(3, "Tamanho", 50, HorizontalAlignment.Left);
+            lsbProdutos.Columns.Insert(4, "Descrição", 350, HorizontalAlignment.Left);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsultar(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnInserir_Click_1(object sender, EventArgs e)
+        {
+
+            if (txtNomeProduto.Text.Trim().Length < 3)
+            {
+                MessageBox.Show("Nome Inválido");
+                txtNomeProduto.Focus();
+
+            }
+
+            else
+            {
+                ListViewItem itmx = lsbProdutos.Items.Add(txtNomeProduto.Text.Trim());
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, txtCor.Text.Trim()));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, cbxSexo.Text.Trim()));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, cbxTamanho.Text.Trim()));
+                itmx.SubItems.Add(new ListViewItem.ListViewSubItem(null, cbxDescricao.Text.Trim()));
+
+                txtNomeProduto.Text = String.Empty;
+                txtCor.Text = String.Empty;
+                cbxDescricao.Text = String.Empty;
+                cbxSexo.Text = String.Empty;
+                cbxTamanho.Text = String.Empty;
+
+            }
+
+
+
+        }
+
+        private void txtNomeProduto_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtNomeProduto_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnInserir_Click_1(btnInserir, new EventArgs());
+            }
+        }
+
+        private void txtNomeProduto_Validating(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void cbxDescricao_Validating(object sender, CancelEventArgs e)
+        {
+          
+        }
+
+        private void cbxSexo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            if (lsbProdutos.FocusedItem.Index < 0)
+            {
+                MessageBox.Show("Nem um item para remover");
+            }
+            else
+            {
+                iSelecionado = lsbProdutos.FocusedItem.Index;
+                lsbProdutos.Items.RemoveAt(iSelecionado);
+                iSelecionado = -1;
+
+            }
+            
+        }
+
+        private void lsbProdutos_Validating(object sender, CancelEventArgs e)
+        {
+            iSelecionado = lsbProdutos.FocusedItem.Index;
+            if (iSelecionado > -1)
+            {
+                btnInserir.Text = ("Alterar");
+            }
+        }
+    }
+}
